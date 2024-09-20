1e182bc0427e0b25f99fcd5e3de0e94125dcd88c
Nikola-Milosavljevic-1 <nikola.milosavljevic@edu.esiee.fr>
2024-09-20 06:17:38 +0000

    Palindrome Nikola Milosavlevic

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 #### Fonction secondaire
-
-
+"""f"""
 def ispalindrome(p):
-
-    # votre code ici
-    
+    """Test"""
+    p=p.lower()
+    mytable=p.maketrans("éèêôçëà","eeeocea","-_ /?', !:")
+    p=p.translate(mytable)
+    if p == p[::-1]:
+        return True
     return False
 
 #### Fonction principale
-
-
 def main():
-
+    """test"""
     # vos appels à la fonction secondaire ici
-
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+    for s in ["raDaR", "kayak"]:
         print(s, ispalindrome(s))
-
 
 if __name__ == "__main__":
     main()
